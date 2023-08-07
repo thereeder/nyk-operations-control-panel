15e1d0ff7584ff690143dc80193bc56292f77cf4
Fernando Gabriel <121118553+thereeder@users.noreply.github.com>
2023-08-07 10:49:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const kingQuestLink = () => {
+    
+    const paragraph = document.querySelector('.warningtext');
+
+    paragraph.style.display = 'block';
+
+    setTimeout(() => {
+        paragraph.style.display = 'none';
+
+    }, 1300)
+}
