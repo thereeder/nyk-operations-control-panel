ca39f4e8ba1d58fe1d34db9b0e4740efcdfd43fc
Fernando Gabriel <121118553+thereeder@users.noreply.github.com>
2023-08-07 16:51:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const goliathLink = '';
-const heroicLink = '';
+const goliathLink = 'https://app.powerbi.com/groups/me/reports/0bc84b68-f321-477d-b3f9-5f9500a4b2e7/ReportSection?experience=power-bi';
+const heroicLink = 'https://app.powerbi.com/groups/me/reports/aa921a29-8a78-447b-ab3b-86f3d2b3a265/ReportSection?experience=power-bi';
 const kingLink = '';
 
 const KingMessage = () => {
@@ -16,7 +16,13 @@ const KingMessage = () => {
 
 const OpenGoliathReport = () => {
 
-    window.open('',
+    window.open(goliathLink,
     "_blank")
 
+}
+
+const OpenHeroicReport = () => {
+
+    window.open(heroicLink,
+    "_blank")
 }
