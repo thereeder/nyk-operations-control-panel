95116e5c1a32ab71c5ef6ba0fcc8aff517d91a00
Fernando Gabriel <121118553+thereeder@users.noreply.github.com>
2024-04-25 08:13:16 -0300

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const graceful_leader_link = 'https://app.powerbi.com/groups/ab3dbb24-3cf5-499b-8fa5-291d8cfde922/reports/c44c8367-e124-433f-92f8-143d43984c8a/ReportSection?experience=power-bi';
+const green_bay_link = 'https://app.powerbi.com/groups/ab3dbb24-3cf5-499b-8fa5-291d8cfde922/reports/bbdaa636-6c74-4649-a1bc-19d4a272adf8/ReportSection?experience=power-bi';
 
 const KingMessage = () => {
     const message = document.querySelector('.kingtext');
@@ -15,6 +16,14 @@ const KingMessage = () => {
 const OpenGracefulReport = () => {
 
     window.open(graceful_leader_link,
+    "_blank")
+
+}
+
+
+const OpenGrenBayReport = () => {
+
+    window.open(green_bay_link,
     "_blank")
 
 }
